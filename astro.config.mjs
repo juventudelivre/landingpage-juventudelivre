9bb831b6e24076157d4bd6115849b8a83ad54b5b
Luccas Sarrassini <luccasarrassini@gmail.com>
2026-07-11 15:33:14 -0300

    chore: add sitemap generation and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import sitemap from "@astrojs/sitemap";
+
 export default defineConfig({
+  site: 'https://juventudelivre.com.br',
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [sitemap()],
 });
